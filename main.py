c8fcab6768e0920034693cab8a26a0db57473e71
Julien <julien.quennehen@edu.devinci.fr>
2022-10-04 20:23:36 +0200

    feat: 🎸 checking for new items present, if yes add them to json
    Need to add better comments

--- main.py
+++ main.py
@@ -1,58 +1,76 @@
 import os
 import requests as rq
 import json
-from dotenv import load_dotenv as env_load
+import sys
+from dotenv import load_dotenv
 
-env_load()
-
-"""
-scraper
-"""
+load_dotenv()
 
 # Get the values of the variables from .env using the os library:
 BASE = os.environ.get("BASE")
 FILTERS = os.environ.get("FILTERS")
 COUNTRY = os.environ.get('COUNTRY')
 
-print("Scrapping in progress ...")
+current_collection = []
+products_added = 0
 page = 1
 
-def scrap(page):
+with open('datas.json', 'r') as f:
+    current_collection = json.load(f)
+    current_collection[::-1]
+
+print("Scrapping in progress ...")
+
+
+def scrap(page, current_collection, products_added):
     url = f"{BASE}/{COUNTRY}/shop/search?q=&page={page}&{FILTERS}"
     request = rq.get(url)
     data = request.json()
-    total_results = []
-    
-    while data is not None:    
-      url = f"{BASE}/{COUNTRY}/shop/search?q=&page={page}&{FILTERS}"  
-      request = rq.get(url)
-      data = request.json()
-      print(f"Scraping page {page}, please wait...")
-      
-      if data['data'] == []:
-          print(f'Successfully scraped everypage {page}')
-          break
-      elif data.status_code != 200:
-          print(f"error happening during request, at page {page}")
-          break
-     
-      # Stores particular details in array
-    
-      for product in data['data']:
-          product_useful = {
-              'id': product['id'], 
-              'brand_id': product['brand_id'],
-              'sku' :product['sku'],
-              'name': product['name'], 
-              'image': product['image']}
-          total_results.append(product_useful)
-    
-      # iterate over all the pages                 
-      page += 1
-      
+
+    while page != 0:
+        url = f"{BASE}/{COUNTRY}/shop/search?q=&page={page}&{FILTERS}"
+        request = rq.get(url)
+        data = request.json()
+        print(f"Scraping page {page}, please wait...")
+
+        if data['data'] == []:
+            print(f'Successfully scraped everypage {page}')
+            break
+        elif request.status_code != 200:
+            print(f"error happening during request, at page {page}")
+            break
+
+        # Stores particular details in array
+        for product in data['data']:
+            if product['id'] == current_collection[products_added]['id']:
+
+                # paste the results into a json file
+                with open('datas.json', 'w') as f:
+                    json.dump(current_collection, f)
+
+                print(f'Number of items found : {products_added} !')
+                print("Collection up to date !")
+                os._exit(os.X_OK)
+
+            else:
+                product_useful = {
+                    'id': product['id'],
+                    'brand_id': product['brand_id'],
+                    'sku': product['sku'],
+                    'name': product['name'],
+                    'image': product['image']}
+
+                current_collection.insert(products_added, product_useful)
+                products_added += 1
+                print(f"Item {product['sku']} added in the collection !")
+
+        # iterate over all the pages
+        page += 1
+
     # paste the results into a json file
     with open('datas.json', 'w') as f:
-      json.dump(total_results, f)
-    
+        json.dump(current_collection, f)
+
+
 if __name__ == '__main__':
-  scrap(page)
+    scrap(page, current_collection, products_added)
